25351721bbb80490d3174c00b6d0d31714eededc
Federico Paolinelli <fedepaol@gmail.com>
2025-02-09 17:57:21 +0100

    Add the controller logic
    The Controller pod is the one responsible for orchestrating the
    configuration of FRR and the network interfaces in the target pod.
    
    Each controller is responsible for the pod running on the same node.
    In the reconciliation it:
    
    - finds the target pod
    - generates the FRR configuration corresponding to the API
    - reloads the FRR configuration by sending a POST call against the
      target pod
    - goes to the target network namespace and applies the configuration
      corresponding to the API

---
+++ internal/controller/frr.go
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/openperouter/openperouter/api/v1alpha1"
+	"github.com/openperouter/openperouter/internal/conversion"
+	"github.com/openperouter/openperouter/internal/frr"
+	"github.com/openperouter/openperouter/internal/frrconfig"
+)
+
+type frrConfigData struct {
+	configFile string
+	address    string
+	port       int
+	nodeIndex  int
+	logLevel   string
+	underlays  []v1alpha1.Underlay
+	vnis       []v1alpha1.VNI
+}
+
+func reloadFRRConfig(ctx context.Context, data frrConfigData) error {
+	slog.DebugContext(ctx, "reloading FRR config", "config", data)
+	frrConfig, err := conversion.APItoFRR(data.nodeIndex, data.underlays, data.vnis, data.logLevel)
+	if err != nil {
+		return fmt.Errorf("failed to generate the frr configuration: %w", err)
+	}
+
+	url := fmt.Sprintf("%s:%d", data.address, data.port)
+	updater := frrconfig.UpdaterForAddress(url, data.configFile)
+	err = frr.ApplyConfig(ctx, &frrConfig, updater)
+	if err != nil {
+		return fmt.Errorf("failed to update the frr configuration: %w", err)
+	}
+	return nil
+}
---
+++ internal/controller/host_config.go
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/openperouter/openperouter/api/v1alpha1"
+	"github.com/openperouter/openperouter/internal/conversion"
+	"github.com/openperouter/openperouter/internal/hostnetwork"
+	"github.com/openperouter/openperouter/internal/pods"
+)
+
+type interfacesConfiguration struct {
+	RouterPodUUID string `json:"routerPodUUID,omitempty"`
+	PodRuntime    pods.Runtime
+	NodeIndex     int                 `json:"nodeIndex,omitempty"`
+	Underlays     []v1alpha1.Underlay `json:"underlays,omitempty"`
+	Vnis          []v1alpha1.VNI      `json:"vnis,omitempty"`
+}
+
+func configureFRRHost(ctx context.Context, config interfacesConfiguration) error {
+	targetNS, err := config.PodRuntime.NetworkNamespace(ctx, config.RouterPodUUID)
+	if err != nil {
+		return fmt.Errorf("failed to retrieve namespace for pod %s: %w", config.RouterPodUUID, err)
+	}
+
+	slog.InfoContext(ctx, "configure interface start", "namespace", targetNS)
+	defer slog.InfoContext(ctx, "configure interface end", "namespace", targetNS)
+	underlayParams, vnis, err := conversion.APItoHostConfig(config.NodeIndex, targetNS, config.Underlays, config.Vnis)
+	if err != nil {
+		return fmt.Errorf("failed to convert config to host configuration: %w", err)
+	}
+
+	slog.InfoContext(ctx, "setting up underlay")
+	if err := hostnetwork.SetupUnderlay(ctx, underlayParams); err != nil {
+		return fmt.Errorf("failed to setup underlay: %w", err)
+	}
+	for _, vni := range vnis {
+		slog.InfoContext(ctx, "setting up VNI", "vni", vni.VRF)
+		if err := hostnetwork.SetupVNI(ctx, vni); err != nil {
+			return fmt.Errorf("failed to setup vni: %w", err)
+		}
+	}
+	return nil
+}
