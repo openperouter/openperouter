f632df0d920789428a6174c0b245db0b5cd9247f
Andreas Karis <ak.karis@gmail.com>
2026-04-08 12:30:25 +0200

    SRv6/ISIS: Lab: add containerlab topology and L3VPN demo examples
    Add new leafSRV6 using SRv6-aware FRR config templates with ISIS
    and segment routing. Configure setup scripts for leafSRV6 with
    loopback IPv6 addresses, seg6 sysctls and VRF strict mode.
    Add L3VPN MetalLB and L3VPN + Layer2 demo examples.
    
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- clab/tools/generate_leaf_config/generate_leafkind.go
+++ clab/tools/generate_leaf_config/generate_leafkind.go
@@ -12,12 +12,30 @@ import (
 	"path/filepath"
 )
 
-type LeafKind1Config struct {
+type LeafKindConfig struct {
 	ASN             int
 	SpineIP         string
 	IPv4ListenRange string
 	IPv6ListenRange string
+	ISISNet         string
 }
+
+const usage = `Usage:
+	generate_leafkind \
+		-leaf <name> \
+		-asn <asn> \
+		-spine-ip <ip> \
+		-ipv4-listen-range <cidr> \
+		-ipv6-listen-range <cidr> \
+		-isis-net <ISIS net> [options]
+Example:
+	generate_leafkind \
+		-leaf leafkind1 \
+		-asn 64512 \
+		-spine-ip 192.168.1.4 \
+		-ipv4-listen-range 192.168.11.0/24 \
+		-ipv6-listen-range 2001:db8:11::/64 \
+		-isis-net 49.0001.0000.0000.0004.00`
 
 func main() {
 	var (
@@ -26,14 +44,15 @@ func main() {
 		spineIP         = flag.String("spine-ip", "", "Spine IP address")
 		ipv4ListenRange = flag.String("ipv4-listen-range", "", "BGP IPv4 listen range (CIDR format)")
 		ipv6ListenRange = flag.String("ipv6-listen-range", "", "BGP IPv6 listen range (CIDR format)")
+		isisNet         = flag.String("isis-net", "", "ISIS net address")
 		outputDir       = flag.String("output", "", "Output directory (default: ../{leaf_name})")
 		templateFile    = flag.String("template", "frr_template/leafkind.conf.template", "Template file path")
 	)
 	flag.Parse()
 
-	if *leafName == "" || *asn == 0 || *spineIP == "" || *ipv4ListenRange == "" || *ipv6ListenRange == "" {
-		fmt.Println("Usage: generate_leafkind -leaf <name> -asn <asn> -spine-ip <ip> -ipv4-listen-range <cidr> -ipv6-listen-range <cidr> [options]")
-		fmt.Println("Example: generate_leafkind -leaf leafkind1 -asn 64512 -spine-ip 192.168.1.4 -ipv4-listen-range 192.168.11.0/24 -ipv6-listen-range 2001:db8:11::/64")
+	if *leafName == "" || *asn == 0 || *spineIP == "" || *ipv4ListenRange == "" || *ipv6ListenRange == "" ||
+		*isisNet == "" {
+		fmt.Println(usage)
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
@@ -42,11 +61,12 @@ func main() {
 		*outputDir = filepath.Join("..", *leafName)
 	}
 
-	config := LeafKind1Config{
+	config := LeafKindConfig{
 		ASN:             *asn,
 		SpineIP:         *spineIP,
 		IPv4ListenRange: *ipv4ListenRange,
 		IPv6ListenRange: *ipv6ListenRange,
+		ISISNet:         *isisNet,
 	}
 
 	if err := GenerateFromTemplate(*templateFile, *outputDir, config); err != nil {
